e1343541e8c180805c50e7485761f9735d8e7a73
zahovicS <zv.elrevelde@gmail.com>
2019-04-26 19:04:12 -0500

    constructor if

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+	$cantidad=0;
+	//PHP es un lenguaje de expresion
+	
+	if(){
+		echo "Este mensaje es si la expresion devuelve true";
+	}
+	else{
+		echo "Este mensaje es si la expresion devuelve false";
+	}
+?>
